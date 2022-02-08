5ea1a06eb0ccb38a4a15e3c04bf93a9bb6f724fd
SolidRamos <99293603+SolidRamos@users.noreply.github.com>
2022-02-08 15:33:25 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,63 @@
+
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.txtmedio', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.btn', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.mega', {
+  duration: 2000,
+  origin: 'top',
+  distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.imgzelda', {
+  duration: 2000,
+  origin: 'top',
+  distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.sunset', {
+  duration: 2000,
+  origin: 'top',
+  distance: '-100px'
+});
+
+
+
+
+
+
+
+
+
+
+
+$(window).resize(function(){
+  winWidth = $(window).width();
+  $("#resize").height(winWidth*.3);
+});
+/*PESTANIA PRODUCTOS*/ 
+
+
+
+
+
